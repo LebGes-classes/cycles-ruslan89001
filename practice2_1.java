35c763dd9ef346996b5929d08c978ef5fc946137
Руслан Никифоров <r4sl18@gmail.com>
2023-09-23 11:09:18 +0300

    practice2 added

---
+++ practice2_1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class practice2_1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double x = scan.nextDouble();
+        double xk = scan.nextDouble();
+        double sh = scan.nextDouble();
+        double y;
+        System.out.println("Значение аргумента   Значение функции");
+        while (x < xk) {
+            if (x >= -9 && x<-6){
+                y = -(Math.pow(-(x*x) - 12 * x - 27, 0.5));
+            }
+            else if (x>=-6 && x<=-3){
+                y = x + 3;
+            }
+            else if (x>-3 && x<=0){
+                y = Math.pow(-(x*x)+9,0.5);
+            }
+            else if (x>0 && x<=3){
+                y = -(x) + 3;
+            }
+            else{
+                y = 0.5*(x - 3);
+            }
+            System.out.println(x+"                      "+y);
+            x+=sh;
+        }
+    }
+}
---
+++ practice2_2.java
@@ -0,0 +1,13 @@
+public class practice2_2 {
+    public static void main(String[] args) {
+        for (int i = 10; i<100; i++){
+            int i1 = i * 2;
+            String str = Integer.toString(i);
+            int s = i % 10 + i / 10;
+            int s1 = i1 % 10 + i1 / 10;
+            if (s == s1){
+                System.out.println(i);
+            }
+        }
+    }
+}
