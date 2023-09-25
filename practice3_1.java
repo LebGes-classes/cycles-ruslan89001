1d7e36087ca46e83679af1b06e93549cbe063502
Руслан Никифоров <r4sl18@gmail.com>
2023-09-25 14:12:43 +0300

    practice3_1

---
+++ practice3_1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class practice3_1 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int m = 0;
+        int res = 0;
+        int sum = 0;
+        boolean f = false;
+        int arr[] = new int[n];
+        for (int i=0;i<n;i++){
+            arr[i] = scan.nextInt();
+            if (Math.abs(arr[i]) > m){
+                res = arr[i];
+                m = Math.abs(arr[i]);
+            }
+        }
+        for (int i=0;i<n;i++){
+            if (arr[i] < 0){
+                f = true;
+                continue;
+            }
+            if (arr[i] < 0){
+                break;
+            }
+            if (f){
+                sum += arr[i];
+            }
+        }
+        System.out.println(res);
+        System.out.println(sum);
+    }
+}
